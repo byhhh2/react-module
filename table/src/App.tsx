0ee38bf51bf80e08d178efe7b11a3375bba942d3
byhhh2 <rlaalsdl111@gmail.com>
2021-10-21 20:21:57 +0900

    Add collapsible table

---
+++ table/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CollapsibleTable from './demo';
+
+function App() {
+  return (
+    <div className="App">
+      <CollapsibleTable />
+    </div>
+  );
+}
+
+export default App;
